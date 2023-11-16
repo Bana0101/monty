853eab858bd706fd87faf7babd81518f86d86ba0
xtreme <anastaai94@gmail.com>
2023-11-16 22:07:05 +0100

    clearing to test - check my repo monty_test for ur files

--- tst.m
+++
@@ -1,9 +0,0 @@
-push 2
-         push        8         
- push          5   
-push   1 
-push   0         mszehrfliuehrl
-                       push                   7               
-                                                                pall 
-								pop
-								pall
