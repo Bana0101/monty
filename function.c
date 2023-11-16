cb402a07858f578f9a637973a759612a4f8adf2f
Ayoub <sb.naba2@gmail.com>
2023-11-16 12:42:27 -0800

    new functions

--- function.c
+++ function.c
@@ -12,13 +12,18 @@ void _push(stack_t **node, unsigned int __attribute__((unused)) line_number)
 	stack_t *ptr;
 
 	if (head == NULL)
+	{
 		head = *node;
+		(*node)->next = NULL;
+		(*node)->prev = NULL;
+	}
 	else
 	{
 		ptr = head;
 		(*node)->next = ptr;
 		ptr->prev = *node;
 		head = *node;
+		(*node)->prev = NULL;
 	}
 }
 
@@ -63,4 +68,38 @@ void _pint(stack_t __attribute__((unused)) **node, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", head->n);
+}
+
+/**
+ * _pop - a function that delete the top element
+ * @node: unused variable
+ * @line_number: the line number
+ * Return: void
+ */
+
+void _pop(stack_t __attribute__((unused)) **node, unsigned int line_number)
+{
+	stack_t *pop;
+
+	if (head == NULL)
+		printf("L%u: can't pop an empty stack", line_number);
+	else
+	{
+		pop = head;
+		head = head->next;
+		head->prev = NULL;
+		free(pop);
+	}
+}
+
+/**
+ * _nop - a function that doesn't do anything
+ * @node: unused variable
+ * @line_number: unused variable
+ * Return: void
+ */
+
+void _nop(stack_t __attribute__((unused)) **node, unsigned int line_number)
+{
+	(void)line_number;
 }
