6718fa11b054c5de615fbf082f15aece48bc7758
xtreme <anastaai94@gmail.com>
2023-11-16 19:48:47 +0100

    files are back

---
+++ tst.m
@@ -0,0 +1,8 @@
+push 2
+         push        8         
+ push          5   
+push   1 
+push   0         mszehrfliuehrl
+                                                                                       pall 9 
+                       push                   7               
+                                                                pall 9 
