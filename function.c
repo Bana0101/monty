b58c7c7eb23ae8a6f749ec393d9f5c198172b1ca
xtreme <anastaai94@gmail.com>
2023-11-17 20:23:04 +0100

    new line on pint func

--- function.c
+++ function.c
@@ -63,7 +63,7 @@ void _pint(stack_t __attribute__((unused)) **node, unsigned int line_number)
 {
 	if (head == NULL)
 	{
-		fprintf(stderr, "L%u: can't pint, stack empty", line_number);
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", head->n);
