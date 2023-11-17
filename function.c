5adc30adabc3ef92a9477b93a28561a011208603
xtreme <anastaai94@gmail.com>
2023-11-17 20:24:54 +0100

    new line on pop func

--- function.c
+++ function.c
@@ -82,7 +82,7 @@ void _pop(stack_t __attribute__((unused)) **node, unsigned int line_number)
 	stack_t *pop;
 
 	if (head == NULL)
-		fprintf(stderr, "L%u: can't pop an empty stack", line_number);
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
 	else
 	{
 		pop = head;
