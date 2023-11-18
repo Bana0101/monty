21195491f2547234b7d9a42bb079a7613e2dccbf
Ayoub <sb.naba2@gmail.com>
2023-11-18 09:15:04 -0800

    check main1 file

--- main.c
+++ main.c
@@ -9,6 +9,9 @@ stack_t *head = NULL;
 int main(int ac, char **av)
 {
 	FILE *file;
+	unsigned int line_count = 0;
+	char **cmd, line[200];
+	int e, i;
 
 	if (ac != 2)
 	{
@@ -21,6 +24,25 @@ int main(int ac, char **av)
 		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
 		exit(EXIT_FAILURE);
 	}
-	principal(file);
+	while (fgets(line, sizeof(line), file) != NULL)
+	{
+		line_count++;
+		cmd = split(line, " ");
+		if (cmd[0][0] == '\n' || cmd[0][0] == '#')
+		{
+			for (i = 0; cmd[i] != NULL; i++)
+				free(cmd[i]);
+			free(cmd);
+			continue;
+		}
+		e = exec(cmd, line_count);
+		for (i = 0; cmd[i] != NULL; i++)
+			free(cmd[i]);
+		free(cmd);
+		if (e)
+			exit(EXIT_FAILURE);
+	}
+	fclose(file);
+	free_dlistint(head);
 	return (0);
 }
